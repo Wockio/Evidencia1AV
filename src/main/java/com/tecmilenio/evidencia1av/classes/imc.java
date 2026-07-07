d1fef9c1172f02097751bf3a00a8a94ed0be3e46
Andres <andres.bros31@hotmail.com>
2026-07-07 15:18:38 -0700

    Se diseñó la clase Imc.java que procesa el cálculo del IMC con los números proporcionados

---
+++ src/main/java/com/tecmilenio/evidencia1av/classes/imc.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tecmilenio.evidencia1av.classes;
+
+/**
+ *
+ * @author andre
+ */
+public class Imc {
+    
+    private double peso;
+    private double altura;
+    private double imc;
+    
+    public double getPeso() {
+        return peso;
+    }
+    
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    
+    public double getAltura() {
+        return altura;
+    }
+    
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+    public double getImc() {
+        return imc;
+    }
+    
+    public void setImc(double imc) {
+        this.imc = imc;
+    }
+    
+    public Imc (String peso, String altura) {
+        this.setPeso(Double.parseDouble(peso));
+        this.setAltura(Double.parseDouble(altura));
+    }
+    
+    public void calcularImc() {
+        this.setImc((this.getPeso()/(this.getAltura()*this.getAltura())));
+    }
+    
+}
